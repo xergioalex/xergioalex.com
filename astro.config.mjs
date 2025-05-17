deb320b5c81860eb94746a8cbdc8e423ecf085ad
Sergio Alexander Florez Galeano <xergioalex@gmail.com>
2025-05-17 15:56:29 -0500

    Setup code check

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://site.xergioalex.com',
-	integrations: [mdx(), sitemap()],
-	server: {
+  site: 'https://site.xergioalex.com',
+  integrations: [mdx(), sitemap(), svelte()],
+  server: {
     host: true,
   },
 });
---
+++ src/env.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly BASE_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
