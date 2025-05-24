5f2944877a6410cb68c69869324650fded1ffe74
Sergio Alexander Florez Galeano <xergioalex@gmail.com>
2025-05-24 10:29:33 -0500

    Updating base content

---
+++ public/scripts/global.theme.js
@@ -0,0 +1,8 @@
+// Theme handling
+const theme = localStorage.getItem('theme');
+if (theme === 'dark') {
+  document.documentElement.classList.add('dark');
+} else {
+  document.documentElement.classList.remove('dark');
+  localStorage.setItem('theme', 'light');
+} 
