40d86ae8e9949cd4bbbd79c5352fdf5db812698d
Sergio Alexander Florez Galeano <xergioalex@gmail.com>
2025-05-24 16:18:04 -0500

    Removing docs

--- docs/scripts/global.theme.js
+++
@@ -1,8 +0,0 @@
-// Theme handling
-const theme = localStorage.getItem('theme');
-if (theme === 'dark') {
-  document.documentElement.classList.add('dark');
-} else {
-  document.documentElement.classList.remove('dark');
-  localStorage.setItem('theme', 'light');
-}
