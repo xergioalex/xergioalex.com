a171cac774fea56b3afa76aa5587968d1eb3d35a
Sergio Alexander Florez Galeano <xergioalex@gmail.com>
2026-02-12 22:27:40 +0000

    New blogging improvements

--- src/components/home/HeroSection/Typewriter.svelte
+++ src/components/home/HeroSection/Typewriter.svelte
@@ -49,6 +49,8 @@ if (words && words.length > 0) {
 }
 </script>
 
-<span class="whitespace-nowrap border-r-2 border-blue-400 pr-1 animate-pulse text-white font-bold text-xl md:text-2xl drop-shadow-lg">
+<span
+  class="whitespace-nowrap border-r-2 border-blue-400 pr-1 animate-pulse text-white font-bold text-lg sm:text-xl md:text-2xl [@media(max-height:760px)]:text-base drop-shadow-lg"
+>
   {display}
 </span> 
